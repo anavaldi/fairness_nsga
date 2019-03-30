2f231cd2aa7f81350abadccc441c3167b827e5c1
your_name <anna.valdivia.garcia@gmail.com>
2019-03-30 20:39:05 +0100

    Adding fairness example

--- fairness.py
+++ fairness.py
@@ -7,10 +7,12 @@ from nsga2.ml import *
 min_range = 0
 max_range = 1
 max_range_depth = 15
+min_range_samples_leaf = 0.0000000000000001
+max_range_samples_leaf = 0.5
 max_leaf_nodes = 1000
 
 
-problem = Problem(num_of_variables = 7, objectives = [accuracy_diff, dem_fpr], variables_range=[(min_range, max_range), (min_range, max_range_depth), (min_range, max_range), (min_range, max_range), (min_range, max_leaf_nodes), (min_range, max_range), (min_range, max_range)])
+problem = Problem(num_of_variables = 7, objectives = [accuracy_diff, dem_fpr], variables_range=[(min_range, max_range), (min_range, max_range_depth), (min_range, max_range), (min_range_samples_leaf, max_range_samples_leaf), (min_range, max_leaf_nodes), (min_range, max_range), (min_range, max_range)])
 evo = Evolution(problem, mutation_param=20)
 func = [i.objective for i in evo.evolve()]
 
