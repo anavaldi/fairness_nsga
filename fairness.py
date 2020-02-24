868d003b0ca82b261609d312e30e64daf20edd2a
anavaldi <anna.valdivia.garcia@gmail.com>
2020-02-24 12:02:22 +0000

    Updating the code

--- fairness.py
+++ fairness.py
@@ -14,15 +14,14 @@ with open('nsga2/config_file.yaml', 'r') as f:
 # problem parameters
 generations = 200
 individuals = 50
-dataset = 'ricci'
-variable = 'Race'
-set_seed = random.randrange(1000)
-
+dataset = 'propublica_violent_recidivism'
+variable = 'race'
+#set_seed = random.randrange(1000)
+set_seed = 24
 
 # write datasets
 X_tr, X_v, X_tst, y_tr, y_v, y_tst = get_matrices(dataset, set_seed)
 write_train_val_test(dataset, set_seed, X_tr, X_v, X_tst, y_tr, y_v, y_tst)
-
 
 # number of rows in train
 num_rows_train = get_matrices(dataset, set_seed)[0].shape[0]
@@ -51,11 +50,14 @@ max_range_impurity_decrease = 0.1
 min_range_class_weight = 1
 max_range_class_weight = 9
 
-#individuals_results = pd.DataFrame()
 
-#problem = Problem(num_of_variables = 7, objectives = [accuracy_inv, dem_fpr], variables_range=[(min_range_criterion, max_range_criterion), (min_range_max_depth, max_range_max_depth), (min_range_samples_split, max_range_samples_split), (min_range_samples_leaf, max_range_samples_leaf), (min_range_leaf_nodes, max_range_leaf_nodes), (min_range_impurity_decrease, max_range_impurity_decrease), (min_range_class_weight, max_range_class_weight)], individuals_df = pd.DataFrame(), num_of_generations = generations, num_of_individuals = individuals, dataset_name = dataset, variable_name = variable)
+# problem with 2 fo
 problem = Problem(num_of_variables = 5, objectives = [gmean_inv, dem_fpr], variables_range = [(min_range_criterion, max_range_criterion), (min_range_max_depth, max_range_max_depth), (min_range_samples_split, max_range_samples_split), (min_range_leaf_nodes, max_range_leaf_nodes), (min_range_class_weight, max_range_class_weight)], individuals_df = pd.DataFrame(), num_of_generations = generations, num_of_individuals = individuals, dataset_name = dataset, variable_name = variable, seed = set_seed)
+
+# problem with 3 fo:
+#problem = Problem(num_of_variables = 5, objectives = [gmean_inv, dem_fpr, complexity], variables_range = [(min_range_criterion, max_range_criterion), (min_range_max_depth, max_range_max_depth), (min_range_samples_split, max_range_samples_split), (min_range_leaf_nodes, max_range_leaf_nodes), (min_range_class_weight, max_range_class_weight)], individuals_df = pd.DataFrame(), num_of_generations = generations, num_of_individuals = individuals, dataset_name = dataset, variable_name = variable, seed = set_seed)
 #problem = Problem(num_of_variables = 5, objectives = [accuracy_inv, dem_fpr], variables_range = [(min_range_criterion, max_range_criterion), (min_range_max_depth, max_range_max_depth), (min_range_samples_leaf, max_range_samples_leaf), (min_range_impurity_decrease, max_range_impurity_decrease), (min_range_class_weight, max_range_class_weight)], individuals_df = pd.DataFrame(), num_of_generations = generations, num_of_individuals = individuals, dataset_name = dataset, variable_name = variable)
+
 evo = Evolution(problem, evolutions_df = pd.DataFrame(), dataset_name = dataset, protected_variable = variable, num_of_generations = generations, num_of_individuals = individuals)
 func = [i.objectives for i in evo.evolve()]
 print(func)
@@ -63,8 +65,8 @@ print(func)
 df = pd.read_csv(config['ROOT_PATH'] + '/results/population/evolution_' + dataset + '_' + variable + '_seed_' + str(set_seed) + '_gen_' + str(generations) + '_indiv_' + str(individuals) + '.csv')
 df = df.loc[(df['rank'] == 0) & ((df['generation'] == 1) | (df['generation'] == 20) | (df['generation'] == 40) | (df['generation'] == 60) | (df['generation'] == 80) | (df['generation'] == 100))]
 
-scatterplot(df, set_seed, 'dem_fp', 'error', 'generation', dataset, variable, generations, individuals)
-
+#scatterplot(df, set_seed, 'dem_fp', 'error', 'generation', dataset, variable, generations, individuals)
+#scatterplot_3d(df, set_seed, 'dem_fp', 'error', 'complexity', 'generation', dataset, variable, generations, individuals)
 
 #x = [i[1] for i in func]
 #y = [i[0] for i in func]
